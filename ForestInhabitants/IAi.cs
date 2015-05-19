28c28da3d2aea204ffc3853e94341cdb092ca6ed
Валерий <v.lihosherstov@gmail.com>
2015-05-19 14:39:19 +0500

    SmartAi alpha

--- ForestInhabitants/IAi.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ForestInhabitants
-{
-	public interface IAi
-	{
-		IEnumerable<Direction> Find(Inhabitant inhabitant, Point aim, Point forestDimensions);
-	}
-}
